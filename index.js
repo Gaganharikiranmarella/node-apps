3ca6050ceead7005edf3b7b6101c9febd9ceacc6
Gaganharikiranmarella <ghk7125@gmail.com>
2025-06-02 10:50:07 +0530

    modified index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 import express from "express"
 const app = express()
-app.listen(8080)
+app.listen(8080, () => {
+    console.log("Server Started. Welcome Gagan!");
+});
+
+app.get("/", (req, res) => {
+    return res.send("Hello World");
+});
