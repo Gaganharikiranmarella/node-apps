1db4c2d354f76912c5b161afbb9ed52fe3fc4181
Gaganharikiranmarella <ghk7125@gmail.com>
2025-06-02 11:44:18 +0530

    assignment done

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ import express from "express"
 const app = express()
 app.listen(8080, () => {
     console.log("Server Started. Welcome Gagan!");
-});
+}); 
 
 app.get("/", (req, res) => {
     return res.send("Hello World");
 });
+
+app.get("/greet",(req,res) => {res.send("Welcome to the website")} )
+app.get("/name", (req,res) => {res/send("Welcome to the browser, Marella Gagan Hari Kiran")} )
