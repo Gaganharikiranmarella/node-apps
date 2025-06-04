721732c54399a4168d2e487f31d7c261157b1339
Gaganharikiranmarella <ghk7125@gmail.com>
2025-06-04 15:36:58 +0530

    changes

--- index.js
+++ index.js
@@ -9,25 +9,37 @@ import orderRoutes from "./routes/orderRoutes.js";
 
 dotenv.config();
 
-const app = express();
+const MONGODB_URI = process.env.MONGODB_URI;
 
+const app = express();
 app.use(cors());
 app.use(express.json());
 
-const connectDB = async () => {
-  if (mongoose.connection.readyState === 0) {
-    await mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(MONGODB_URI)
+  .then(() => {
     console.log("MongoDB connected");
-  }
-};
-
-app.use(async (req, res, next) => {
-  await connectDB();
-  next();
-});
+    app.listen(8080, () => {
+      console.log("Server started on http://localhost:8080");
+    });
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
 
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+app.get("/greet", (req, res) => {
+  res.send("Welcome to the website");
+});
+
+app.get("/name", (req, res) => {
+  res.send("Welcome to the browser, Marella Gagan Hari Kiran");
+});
+
+app.get("/weather", (req, res) => {
+  res.json({ temperature: "41°C" });
 });
 
 app.use("/users", userRoutes);
